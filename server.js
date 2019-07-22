ffd1ee23bb8122eaf09f3688f02b367e267c6ffc
Henry Lay <henry88lay@gmail.com>
2019-07-22 21:55:25 +1000

    Feat: Added route folder and files
    Added 3 files:
    auth.js
    contacts.js
    users.js
    connected to server.js

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) =>
+  res.json({msg: 'Welcome to the ContactKeeper API...'})
+);
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
